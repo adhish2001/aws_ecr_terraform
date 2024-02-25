add7598985ccf7e1ab68527991213950d908000c
Adhish Maheswaran <amahe023@uottawa.ca>
2024-02-24 23:02:37 -0500

    added default to variable ecr_repo_name

--- variable.tf
+++ variable.tf
@@ -1,10 +1,10 @@
 variable "ecr_repo_name" {
     type        = string
     description = "The name variable used for the ecr repository"
+    default = "default-ecr-repo"
 }
 variable "aws_region" {
     type        = string
     description = "The AWS region"
     default     = "us-east-2"
-  
 }
